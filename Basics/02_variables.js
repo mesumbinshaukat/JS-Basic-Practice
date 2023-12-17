4a9083467d5fdbcf123595e356ec5cdd7e07f4c6
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-17 12:36:29 +0000

    Variables And Datatypes

---
+++ Basics/02_variables.js
@@ -0,0 +1,6 @@
+const userId = Math.floor(Math.random() * 10)
+let userName = "Mesum"
+var userPassword = "HelloWorld!" //var keyword is the bad practice for making variables.
+userNumber = "03362100225"
+
+console.table([userId, userName, userPassword, userNumber])
---
+++ Basics/03_standard_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; //It strictly follows new code practices in JS according to the latest version of JS.
+
+let userName = "Mesum" // Datatype = String
+let userPassword = 778866 // Datatype = Number
+let userAge = 18.7 // Datatype = Number
+let isUserActive = true // Datatype = boolean
+let userCountry // Datatype = undefined
+let userWeight = null // Datatype = Object, null is also known as standalone value
+
+console.table([typeof userName, typeof userPassword, typeof userAge, typeof isUserActive, typeof userCountry, typeof userWeight])
