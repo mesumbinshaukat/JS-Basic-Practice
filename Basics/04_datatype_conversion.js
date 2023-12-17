c12cc2be5d1d3b87c8a2ed3f29dd9f1772b95558
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-17 13:01:10 +0000

    Datatype Conversion

---
+++ Basics/04_datatype_conversion.js
@@ -0,0 +1,45 @@
+let age = 13
+
+let string_Age = String(age)
+
+console.table([typeof age, age, typeof string_Age, string_Age])
+
+let randomNumber = Math.floor(Math.random() * 100)
+
+let boolean_Number = Boolean(randomNumber)
+
+console.table([typeof randomNumber, randomNumber, typeof boolean_Number, boolean_Number])
+
+let username = "M3SU^"
+
+let number_Name = Number(username)
+
+console.table([typeof username, username, typeof number_Name, number_Name])
+
+let one = 1
+
+let boolean_One = Boolean(one)
+
+let zero = 0
+
+let boolean_Zero = Boolean(zero)
+
+console.table([typeof one, one, typeof boolean_One, boolean_One, typeof zero, zero, typeof boolean_Zero, boolean_Zero])
+
+let empty = ""
+
+let boolean_Empty = Boolean(empty)
+
+console.table([typeof empty, empty, typeof boolean_Empty, boolean_Empty])
+
+let isTrue = true
+
+let number_IsTrue = Number(isTrue)
+
+console.table([typeof isTrue, isTrue, typeof number_IsTrue, number_IsTrue])
+
+let isFalse = false
+
+let number_IsFalse = Number(isFalse)
+
+console.table([typeof isFalse, isFalse, typeof number_IsFalse, number_IsFalse])
