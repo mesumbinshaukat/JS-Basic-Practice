be74e8eeb31a74374488a6aee800ff30c1d745a8
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-17 14:13:32 +0000

    Arthmetic Operation

---
+++ Basics/05_arithmetic_operation.js
@@ -0,0 +1,26 @@
+let num1 = 10
+let num2 = 15
+
+let result1 = num1 + num2
+let result2 = num1 - num2
+let result3 = num1 * num2
+let result4 = num1 * num2
+let result5 = num1 / num2
+let result6 = num1 ** num2
+let result7 = num1 % num2
+
+console.table([result1, result2, result3, result4, result5, result6, result7])
+
+let num3 = +true
+let num4 = +false
+
+console.table([num3, num4])
+
+let num5 = 6
+
+// let result8 = ++num5
+// let result9 = num5++
+let result10 = --num5
+let result11 = num5--
+
+console.table([/*result8, result9, */result10, result11])
