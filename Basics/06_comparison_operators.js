a29282017679dc1d9c8fb8191a6d52d1e1ab5ffa
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-17 14:28:22 +0000

    Comparison Operators

---
+++ Basics/06_comparison_operators.js
@@ -0,0 +1,19 @@
+const check1 = 2 > 3 // False
+const check2 = 4 < 6 // True
+const check3 = 4 == 5 // False
+const check4 = 4 != 5 // True
+const check5 = 10 >= 10 // True
+const check6 = 11 <= 20 // True
+const check7 = "2" == 2 // True
+const check8 = "2" === 2 // False
+const check9 = "2" === "2" // True
+const check10 = null == 0 // False
+const check11 = null >= 0 //  True
+const check12 = null <= 0 // True
+const check13 = null === 0 // False
+const check14 = null != 0 // True
+const check15 = null >= "0" // True
+
+console.table([check1, check2, check3, check4, check5, check6, check7, check8, check9, check10, check11, check12, check13, check14, check15])
+
+
