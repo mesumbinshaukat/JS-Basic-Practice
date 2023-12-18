1626a16707d3b36e0be43ba4a8fb3b676d47f330
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-18 21:26:52 +0000

    Basic function concepts

--- 03_Core Concepts Of Basics/01_functions.js
+++ 03_Core Concepts Of Basics/01_functions.js
@@ -27,3 +27,28 @@ function basicCondition(name){
     return `Hello, ${name}`;
 }
 console.log(basicCondition("Mesum"));
+
+function calculateQty(qty1, qty2, ...allQty){ //This ... is called rest operator
+    return `First quantity: ${qty1} \nSecond quantity: ${qty2} \nAll Quantity: ${allQty}`
+}
+console.log(calculateQty(200, 500, 3000, 900, 34500, 10035));
+
+const User = {
+    name: "Johnson",
+    age: 18,
+    course: "Backend Web Development"
+}
+
+function userInfo(obj){
+    return `Hello, ${obj.name}! You're ${obj.age} years old and you're enrolled in ${obj.course} course.`
+}
+
+console.log(userInfo(User));
+
+const numbers = [100, 200, 500, 600]
+
+function myNumber(obj){
+    return obj[2]
+}
+
+console.log(myNumber(numbers));
