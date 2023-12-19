69d0e4e8f6cf408420bd41cd316dd9908c78f687
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-19 20:33:12 +0000

    For Each Loop

---
+++ 05_Loops/05_for_each_loop.js
@@ -0,0 +1,57 @@
+const names = ["John", "Winston", "William", "Alfred", "Donnie"]
+
+let count = 1
+names.forEach(function (name) {
+    // console.log(`${count}) ${name}`);
+    count++
+})
+
+count = 1
+
+names.forEach((name)=>{
+    // console.log(`${count}) ${name}`);
+    count++
+})
+
+count = 1
+
+function fName(name){
+    // console.log(`${count}) ${name}`);
+    count++
+}
+
+names.forEach(fName)
+
+count = 1
+names.forEach((name) => (
+    // console.log(`${count}) ${name}`),
+    count++
+))
+
+
+// names.forEach((name, index, arr) => (
+//     console.log(`${index}) ${name} <=:(-):=> ${arr}`)
+// ))
+
+const proLang = [
+    {
+        langName: "JavaScript",
+        langExtention: "js"
+    },
+    {
+        langName: "Python",
+        langExtention: "py"
+    },
+    {
+        langName: "Ruby",
+        langExtention: "rb"
+    },
+    {
+        langName: "Objective-C",
+        langExtention: "c"
+    }
+]
+
+proLang.forEach((params) => {
+    console.table([`Programming Language:- ${params.langName}`, `${params.langName} extention:- ${params.langExtention}`])
+})
