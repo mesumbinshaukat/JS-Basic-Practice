e04ba73908c9ffde429b4bb7aba20dbb7d09d58f
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-12-19 18:37:04 +0000

    While & Do While Loop

---
+++ 05_Loops/02_while_and_do_while.js
@@ -0,0 +1,22 @@
+let i = 0
+
+while (i <= 10) {
+    console.log(i);
+    i++
+}
+
+let arr = ["Superman", "Ironman", "Batman", "Flash", "Aquaman", "Green lantern", "Captain America"]
+console.log(arr.length);
+
+let x = 0
+while(x < arr.length){
+    console.log(arr[x]);
+    x++
+}
+
+let score = 20
+
+do {
+    console.log(score);
+    score++
+} while (score <= 10);
